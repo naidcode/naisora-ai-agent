a8a219a0148e5101635337e6756317d3558758c1
naidcode <naidcode.dev@gmail.com>
2026-04-21 16:33:34 +0530

    fix folder structure

--- agency-ai-agent/scripts/start.js
+++
@@ -1,4 +0,0 @@
-// scripts/start.js
-const { startAgent } = require('../system/masterSwitch');
-startAgent();
-console.log('Agent has been activated.');
--- agency-ai-agent/scripts/stop.js
+++
@@ -1,4 +0,0 @@
-// scripts/stop.js
-const { stopAgent } = require('../system/masterSwitch');
-stopAgent();
-console.log('Agent has been deactivated.');
